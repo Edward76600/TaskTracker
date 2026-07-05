ec7f7457152df0998ec075cdff9bf6f9e8abfa17
Edward <127325313+Edward76600@users.noreply.github.com>
2026-07-05 21:14:50 +0200

    commit

--- src/tasktracker/api/routes.py
+++ src/tasktracker/api/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from tasktracker.models.core.completion_status import CompletionStatus
 from tasktracker.models.host.task_request import TaskRequest
 from tasktracker.models.core.task import Task
 from tasktracker.resource_access.task_resource_access import TaskResourceAccess
@@ -13,5 +14,14 @@ def list_tasks() -> list[Task]:
 
 
 @router.post("/tasks", tags=["tasks"], status_code=201)
-def create_task(task:TaskRequest):
+def create_task(task: TaskRequest) -> Task:
     return task_resource_access.add_task(task)
+
+
+@router.delete("/tasks/{task_id}", tags=["tasks"], status_code=204)
+def delete_task(task_id: str) -> None:
+    task_resource_access.delete_task(task_id)
+
+@router.put("/tasks/{task_id}", tags=["tasks"], status_code=200)
+def update_task(task_id: str, status: CompletionStatus) -> Task:
+    return task_resource_access.update_task(task_id, status)
--- src/tasktracker/resource_access/task_resource_access.py
+++ src/tasktracker/resource_access/task_resource_access.py
@@ -1,6 +1,9 @@
-from tasktracker.resource_access.repository import task_tracker_repository
+import datetime as dt
+
+from tasktracker.models.core.completion_status import CompletionStatus
 from tasktracker.models.core.task import Task
 from tasktracker.models.host.task_request import TaskRequest
+from tasktracker.resource_access.repository import task_tracker_repository
 
 
 class TaskResourceAccess:
@@ -20,3 +23,18 @@ class TaskResourceAccess:
         data.append(task.model_dump(mode="json"))
         self.repo.save_data_json(data)
         return task
+
+    def delete_task(self, task_id: str) -> None:
+        data = self.repo.load_data_json()
+        data = [t for t in data if t["task_id"] != task_id]
+        self.repo.save_data_json(data)
+
+    def update_task(self, task_id: str, status: CompletionStatus) -> Task:
+        data = self.repo.load_data_json()
+        updated_task = next((t for t in data if t["task_id"] == task_id), None)
+        if updated_task is None:
+            raise ValueError(f"Task {task_id} not found")
+        updated_task["completion_status"] = status.value
+        updated_task["update_date"] = dt.date.today().isoformat()
+        self.repo.save_data_json(data)
+        return Task.model_validate(updated_task)
